dae2c007e790b2c1d68fd700db663a7add040554
TarkWight <arsentiy.gonda@gmail.com>
2026-09-19 15:39:47 +0700

    core/domain: add time implementation

---
+++ src/Domain/Common/Time.cpp
@@ -0,0 +1,137 @@
+#include <Domain/Common/Time.hpp>
+
+namespace domain {
+
+namespace {
+
+constexpr Seconds minRequiredDuration{1};
+constexpr auto maxDuration = std::chrono::hours{24};
+
+} // namespace
+
+TestDurationCreationResult TestDuration::required(Seconds requested) {
+    if (requested < minRequiredDuration) {
+        return {
+            TestDuration{minRequiredDuration},
+            TimeAdjustment{
+                .requested = requested,
+                .applied = minRequiredDuration,
+                .reason = TimeAdjustmentReason::BelowMinimum,
+            },
+        };
+    }
+
+    if (requested > maxDuration) {
+        const auto applied = std::chrono::duration_cast<Seconds>(maxDuration);
+
+        return {
+            TestDuration{applied},
+            TimeAdjustment{
+                .requested = requested,
+                .applied = applied,
+                .reason = TimeAdjustmentReason::AboveMaximum,
+            },
+        };
+    }
+
+    return {
+        TestDuration{requested},
+        std::nullopt,
+    };
+}
+
+TestDurationCreationResult TestDuration::optional(Seconds requested) {
+    constexpr Seconds minimum{0};
+
+    if (requested < minimum) {
+        return {
+            TestDuration{minimum},
+            TimeAdjustment{
+                .requested = requested,
+                .applied = minimum,
+                .reason = TimeAdjustmentReason::NegativeValue,
+            },
+        };
+    }
+
+    if (requested > maxDuration) {
+        const auto applied = std::chrono::duration_cast<Seconds>(maxDuration);
+
+        return {
+            TestDuration{applied},
+            TimeAdjustment{
+                .requested = requested,
+                .applied = applied,
+                .reason = TimeAdjustmentReason::AboveMaximum,
+            },
+        };
+    }
+
+    return {
+        TestDuration{requested},
+        std::nullopt,
+    };
+}
+
+TestDuration::TestDuration(Seconds value) noexcept : value_(value) {
+}
+
+Seconds TestDuration::value() const noexcept {
+    return value_;
+}
+
+ElapsedTimeCreationResult ElapsedTime::from(Seconds requested) {
+    constexpr Seconds minimum{0};
+
+    if (requested < minimum) {
+        return {
+            ElapsedTime{minimum},
+            TimeAdjustment{
+                .requested = requested,
+                .applied = minimum,
+                .reason = TimeAdjustmentReason::NegativeValue,
+            },
+        };
+    }
+
+    return {
+        ElapsedTime{requested},
+        std::nullopt,
+    };
+}
+
+ElapsedTime::ElapsedTime(Seconds value) noexcept : value_(value) {
+}
+
+Seconds ElapsedTime::value() const noexcept {
+    return value_;
+}
+
+RemainingTimeCreationResult RemainingTime::from(Seconds requested) {
+    constexpr Seconds minimum{0};
+
+    if (requested < minimum) {
+        return {
+            RemainingTime{minimum},
+            TimeAdjustment{
+                .requested = requested,
+                .applied = minimum,
+                .reason = TimeAdjustmentReason::NegativeValue,
+            },
+        };
+    }
+
+    return {
+        RemainingTime{requested},
+        std::nullopt,
+    };
+}
+
+RemainingTime::RemainingTime(Seconds value) noexcept : value_(value) {
+}
+
+Seconds RemainingTime::value() const noexcept {
+    return value_;
+}
+
+} // namespace domain
